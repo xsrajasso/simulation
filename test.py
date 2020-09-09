20dcb7102c3bc30d3f2e5526de5b0ad7b07b7627
xsrajasso <xsrajasso@DESKTOP-4EI7FQS.localdomain>
2020-09-09 15:41:21 -0500

    cambios menores

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 data = [5, 4, 5, 6, 7, 9, 39, 25, 1, 2, 4, 15]
 import numpy as np
 from scipy.stats import describe
-np.mean(data)
-np.median(data)
-describe(data)
+print(np.mean(data))
+print(np.median(data))
+print(describe(data))
