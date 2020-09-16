fe7dcf99821e5442461ff9dc4d7a268b428f8697
xsrajasso <62636466+xsrajasso@users.noreply.github.com>
2020-09-16 13:23:09 -0500

    still not working

--- tarea1/tarea1.py
+++ tarea1/tarea1.py
@@ -1,22 +1,23 @@
-from random import random, randint
+from random import getrandbits, randint
+import matplotlib.pyplot as plt
+from math import sqrt, fabs
 
-def paso(pos, dim):
-    d = randint(0, dim - 1)
-    pos[d] += -1 if random() < 0.5 else 1
-    return pos
-
-def experimento(largo, dim):
+def experimento(dim, dur, eucl):
     pos = [0] * dim
-    for t in range(largo):
-        pos = paso(pos, dim)
-        if all([p == 0 for p in pos]):
-            return True
-    return False
-
-dim = 8
-largo = 1000
-total = 10000
-regresos = 0
-for replica in range(total):
-    regresos += experimento(largo, dim)
-print (regresos / total)
+    mayor = 0
+    delta = getrandbits(dur)
+    for t in range(dur):
+        pos[randint(0, dim - 1)] += 1 if delta % 2 == 0 else -1
+        delta >>= 1
+    mayor = max(mayor, sqrt(sum([p**2 for p in pos])) if eucl else sum([fabs(p) for p in pos]))
+    return mayor
+if __name__ == "__main__":
+    dimension = [d for d in range(1, 9)] # de uno a ocho
+    duracion = 200
+    replicas = 100
+    results = None
+    p = [(d, duracion, True) for d in dimension]
+    p += [(d, duracion, False) for d in dimension]
+    param = p * replicas
+    results =(experimento, param)
+print(results)
